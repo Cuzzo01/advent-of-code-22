30ef494628ec62cce3fa74c4c0875ef358056fab
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-12-05 07:47:08 -0600

    Day 5

---
+++ day-5/day-5-pt2.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt", "utf-8");
+const lines = input.split("\n");
+
+const stackRows = [];
+let firstInstructionLine = 0;
+for (const [index, line] of lines.entries()) {
+  if (line.at(1) === "1") {
+    firstInstructionLine = index + 2;
+    break;
+  }
+  const row = [];
+  for (let i = 0; i < line.length - 1; i += 4) {
+    const currentChar = line.at(i);
+    const nextChar = line.at(i + 1);
+
+    if (currentChar === "[") {
+      row.push(nextChar);
+    } else if (currentChar === " ") {
+      if (nextChar === " ") row.push("");
+    }
+  }
+  stackRows.push(row);
+}
+
+const numStacks = stackRows[0].length;
+const stacks = {};
+for (let i = 0; i < numStacks; i++) {
+  const stack = [];
+  for (let j = 0; j < stackRows.length; j++) {
+    const char = stackRows[j][i];
+    if (char !== "") stack.push(stackRows[j][i]);
+  }
+  stacks[i + 1] = stack.reverse();
+}
+
+for (let i = firstInstructionLine; lines[i]; i++) {
+  const line = lines[i];
+  const instructionArr = line.split(" ");
+  const numToMove = instructionArr[1];
+  const fromStackNum = instructionArr[3];
+  const toStackNum = instructionArr[5];
+
+  const fromStack = stacks[fromStackNum];
+  const cratesToBeMoved = [];
+  for (let moveNum = 0; moveNum < numToMove; moveNum++) {
+    cratesToBeMoved.push(fromStack.pop());
+  }
+
+  while (cratesToBeMoved.length !== 0) {
+    stacks[toStackNum].push(cratesToBeMoved.pop());
+  }
+}
+
+let solution = "";
+for (const [key, value] of Object.entries(stacks)) {
+  const top = value.pop();
+  solution += top;
+}
+
+console.log(solution);
