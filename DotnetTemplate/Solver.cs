1ef488f2acbfe89d79eb5f8737734961c1564492
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2022-12-07 19:54:58 -0600

    Add template for solving with C#

---
+++ DotnetTemplate/Solver.cs
@@ -0,0 +1,19 @@
+namespace AoCTemplate {
+    class Solver {
+        public Solver(string input) {
+            ParseInput(input);
+        }
+
+        public string Part1() {
+            return "Not implemented";
+        }
+
+        public string Part2() {
+            return "Not implemented";
+        }
+
+        private void ParseInput(string input) {
+            var lines = input.Split("\n");
+        }
+    }
+}
